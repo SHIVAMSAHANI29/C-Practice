6fac7269c89036d1eaba211fefc2084f59b83fe5
SHIVAM SAHANI <shivamsahaniddu2507@gmail.com>
2026-08-12 20:42:33 +0530

    Add C++ practice programs

---
+++ Recursion/recursion_factorial.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if(n<0)
+        return -1;
+    else if (n==0 || n==1)
+    {
+        return 1;
+    }
+    else
+        return n*fact(n-1);
+    
+}
+
+int main()
+{
+    int n;
+    cout<<("Enter the number :");
+    cin>>n;
+
+    cout<<"Factorial  is = "<<fact(n)<<endl;
+    return 0;
+}
--- majority_elements3.cpp
+++ majority_elements3.cpp
@@ -3,26 +3,26 @@
 #include<algorithm>//used for sorting
 using namespace std;
 
-// int majorityElements(vector<int>&nums){
-//     //sort(nums.begin(),nums,end());//inbuilt function to sort teh array
-//     int n = nums.size();
-//      int freq = 1;
-//      int ans = nums[0];
+int majorityElements(vector<int>&nums){
+    //sort(nums.begin(),nums,end());//inbuilt function to sort teh array
+    int n = nums.size();
+     int freq = 1;
+     int ans = nums[0];
 
-//      for(int i=1;i<n;i++){
-//         if(nums[i] ==nums[i-1]){
-//             freq++ ;
-//         }
-//         else{
-//             freq =1;
-//             ans=nums[i];   
-//         }
-//      }
-//      if(freq > n/2){
-//         return ans;
-//      }
+     for(int i=1;i<n;i++){
+        if(nums[i] ==nums[i-1]){
+            freq++ ;
+        }
+        else{
+            freq =1;
+            ans=nums[i];   
+        }
+     }
+     if(freq > n/2){
+        return ans;
+     }
 
-// }
+}
 int main(){
     vector<int>nums={3,2,3};
     int n= 3;
